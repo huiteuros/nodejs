4bd37caa23155c9d921856aa3e9472f88d13a3ad
huiteuros <gabriel.amphoux@gmail.com>
2021-12-16 09:42:58 +0100

    initialisation du projet

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 5000 // this is very important
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(PORT, function () {
+  console.log('Example app listening on port ' + PORT)
+})
