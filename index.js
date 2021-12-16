d0af5c5ab4d087a763df11f56eee8e25a38b7e24
huiteuros <gabriel.amphoux@gmail.com>
2021-12-16 12:27:02 +0100

    debut du projet noté

--- index.js
+++ index.js
@@ -3,9 +3,30 @@ const app = express()
 const PORT = process.env.PORT || 5000 // this is very important
 
 app.get('/', function (req, res) {
-  res.send('Hello World :) !')
+  res.send('Home page')
 })
 
 app.listen(PORT, function () {
   console.log('Example app listening on port ' + PORT)
 })
+
+const axios = require('axios');
+
+app.get('/plats', function (req, res) {
+  axios.get('https://tpnotenodejsaxellebg-46db.restdb.io/rest/plats', { 
+  headers: 
+   { 'cache-control': 'no-cache',
+     'x-apikey': 'fb59b6db4a85575897bbf2f4e44772075146f' } 
+   })
+  .then(result => res.json(result.data))
+})
+
+app.get('/plats/:id', function (req, res) {
+    axios.get('https://tpnotenodejsaxellebg-46db.restdb.io/rest/plats'+res.id, { 
+    headers: 
+     { 'cache-control': 'no-cache',
+       'x-apikey': 'fb59b6db4a85575897bbf2f4e44772075146f' } 
+     })
+    .then(result => res.json(result.data))
+  })
+
