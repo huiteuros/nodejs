cda05aa2c136d17c5224ef2de423c87531c3a7ac
Gabriel Amphoux <38751848+huiteuros@users.noreply.github.com>
2022-03-15 17:20:53 +0100

    Mise en place des routes pour les plats
    CRUD des plats

--- index.js
+++ index.js
@@ -1,32 +1,48 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000 // this is very important
+const HEADER = { 'cache-control': 'no-cache', 'x-apikey': 'fb59b6db4a85575897bbf2f4e44772075146f' }
+const ADDRESS = 'https://tpnotenodejsaxellebg-46db.restdb.io/rest'
+const cors = require('cors')
+
+app.use(
+    express.json(),
+    express.urlencoded({ extended: true }),
+    cors(),
+)
+app.listen(PORT, function () {
+    console.log('Example app listening on port ' + PORT)
+})
 
 app.get('/', function (req, res) {
   res.send('Home page')
 })
 
-app.listen(PORT, function () {
-  console.log('Example app listening on port ' + PORT)
-})
-
 const axios = require('axios');
 
 app.get('/plats', function (req, res) {
-  axios.get('https://tpnotenodejsaxellebg-46db.restdb.io/rest/plats', { 
-  headers: 
-   { 'cache-control': 'no-cache',
-     'x-apikey': 'fb59b6db4a85575897bbf2f4e44772075146f' } 
-   })
+  axios.get(ADDRESS+'/plats', {
+  headers: HEADER })
   .then(result => res.json(result.data))
 })
 
 app.get('/plats/:id', function (req, res) {
-    axios.get('https://tpnotenodejsaxellebg-46db.restdb.io/rest/plats'+res.id, { 
-    headers: 
-     { 'cache-control': 'no-cache',
-       'x-apikey': 'fb59b6db4a85575897bbf2f4e44772075146f' } 
-     })
+    axios.get(ADDRESS+'/plats/'+req.params.id, {
+    headers: HEADER })
     .then(result => res.json(result.data))
-  })
+})
 
+app.post('/ajouterPlat', function(req, res) {
+    axios.post(ADDRESS+'/plats', req.body, {headers : HEADER})
+            .then(result => res.json("Plat ajouté"))
+})
+
+app.post('/supprimerPlat/:id', function(req, res){
+    axios.delete(ADDRESS+'/plats/'+req.params.id, {headers : HEADER})
+        .then(result => res.json("Plat supprimé"))
+})
+
+app.put('/modifierPlat/:id', function(req, res) {
+    axios.put(ADDRESS+'/plats/'+req.params.id, req.body, {headers : HEADER})
+        .then(result => res.json("Plat modifié"))
+})
