ac93ee9400ced672ca096cdf4ca7a6fcc8606599
Gabriel Amphoux <38751848+huiteuros@users.noreply.github.com>
2022-03-16 11:35:34 +0100

    V1 tpnote nodeJS API rest

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 5000 // this is very important
+const PORT = process.env.PORT || 3000
 const HEADER = { 'cache-control': 'no-cache', 'x-apikey': 'fb59b6db4a85575897bbf2f4e44772075146f' }
-const ADDRESS = 'https://tpnotenodejsaxellebg-46db.restdb.io/rest'
+const ADDRESS = 'https://tpnotenodejsaxellebg-46db.restdb.io/rest'// Désolé pour le nom de la BDD, on avait trouvé ça drole en tp et on a pas réussi à le changer
 const cors = require('cors')
 const passport = require('passport')
 const jwt = require('jsonwebtoken')
 const passportJWT = require('passport-jwt')
-const secret = 'testdusecret'
+const secret = 'motmagikbogoss'
 const ExtractJwt = passportJWT.ExtractJwt
 const JwtStrategy = passportJWT.Strategy
 
@@ -63,9 +63,15 @@ app.get('/plats/:id', function (req, res) {
     .then(result => res.json(result.data))
 })
 
-app.post('/ajouterPlat', passport.authenticate('jwt', { session: false }), function(req, res) {
+// Cette route est censée nous permettre d'ajouter un plat uniquement si nous sommes connectés. Mais on n'a pas réussi à la tester avec postman
+app.post('/ajouterPlatV2', passport.authenticate('jwt', { session: false }), function(req, res) {
     axios.post(ADDRESS+'/plats', req.body, {headers : HEADER})
             .then(result => res.json("Plat ajouté"))
+})
+
+app.post('/ajouterPlat', function(req, res) {
+    axios.post(ADDRESS+'/plats', req.body, {headers : HEADER})
+        .then(result => res.json("Plat ajouté"))
 })
 
 app.post('/supprimerPlat/:id', function(req, res){
@@ -78,7 +84,7 @@ app.put('/modifierPlat/:id', function(req, res) {
         .then(result => res.json("Plat modifié"))
 })
 
-app.post('/login', async (req, res) => {
+app.post('/connexion', async (req, res) => {
     const email = req.body.email
     const mdp = req.body.mdp
 
@@ -101,6 +107,11 @@ app.post('/login', async (req, res) => {
 
     const userJwt = jwt.sign({email: user.email}, secret)
 
-    res.json({ jwt: userJwt })
+    res.json({ jwt: userJwt, user: user })
 
+})
+
+app.post('/inscription', function(req, res) {
+    axios.post(ADDRESS+'/users', req.body, {headers : HEADER})
+        .then(result => res.json("User ajouté"))
 })
